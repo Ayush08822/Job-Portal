cc8b02b4e3958daf763a6858aa4f598e5b7eb4c3
Ayush <imak070504@gmail.com>
2026-01-14 00:05:51 +0530

    Corrected the folder structure.

--- JobPortal/src/main/java/com/AyushToCode/JobPortal/repository/JobSeekerProfileRepository.java
+++
@@ -1,7 +0,0 @@
-package com.AyushToCode.JobPortal.repository;
-
-import com.AyushToCode.JobPortal.entity.JobSeekerProfile;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface JobSeekerProfileRepository extends JpaRepository<JobSeekerProfile,Integer> {
-}
--- JobPortal/src/main/java/com/AyushToCode/JobPortal/repository/UserTypeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.AyushToCode.JobPortal.repository;
-
-import com.AyushToCode.JobPortal.entity.UsersType;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserTypeRepository extends JpaRepository<UsersType,Integer>{
-}
